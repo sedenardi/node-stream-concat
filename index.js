293c3d6c5b39ca6a489b070595b7a773ead7c7bf
Alex Ehrnschwender <alexanderscott46@gmail.com>
2015-01-09 17:27:59 -0800

    remove logic from constructor, remove counter refs, wrap in async candy

--- index.js
+++ index.js
@@ -1,47 +1,66 @@
 var Transform = require('stream').Transform,
-    util = require('util');
+  async = require('async'),
+  _und = require('underscore'),
+  util = require('util');
 
-var StreamConcat = function(streams, options) {
+function StreamError(msg, constr) {
+  Error.captureStackTrace(this, constr || this);
+  this.message = msg;
+}
+
+util.inherits(StreamError, Error);
+StreamError.prototype.name = 'Stream Error';
+
+function StreamConcat(streams, options) {
   Transform.call(this, options);
 
-  var self = this;
-
   this.streams = streams;
-  this.canAddStream = true;
-  this.currentStream = null;
-  this.streamIndex = 0;
-  var nextStream = function() {
-    self.currentStream = null;
-    if (self.streams.constructor === Array && self.streamIndex < self.streams.length) {
-      self.currentStream = self.streams[self.streamIndex++];
-    } else if (typeof self.streams === 'function') {
-      this.canAddStream = false;
-      self.currentStream = self.streams();
-    }
 
-    if (self.currentStream === null) {
-      this.canAddStream = false;
-      self.push(null);
-    } else {
-      self.currentStream.pipe(self, {end: false});
-      self.currentStream.on('end', nextStream);
-    }
-  };
-
-  nextStream();
-};
+  return this;
+}
 
 util.inherits(StreamConcat, Transform);
 
+StreamConcat.prototype.run = function(cb){
+  var self = this;
+  cb = (cb && _und.isFunction(cb)) ? cb : _und.noop();
+  return async.doWhilst(
+    function(cb){
+      var stream;
+      if(_und.isArray(self.streams)) stream = self.streams.shift();
+      else if(_und.isFunction(self.streams)) stream = self.streams();
+      else return cb(new StreamError("Unknown stream type"));
+
+      stream.pipe(self, { end: false });
+      stream.on('end', cb);
+    },
+    function(){ return !_und.isEmpty(self.streams); }, 
+    function(err){
+      if(err){
+        self.emit('error', err);
+        return cb(err);
+      }
+      self.push(null);
+      return cb(null);
+    }
+  );
+};
+
+  
 StreamConcat.prototype._transform = function(chunk, encoding, callback) {
   callback(null, chunk);
 };
 
+StreamConcat.prototype._canAddStream = function() {
+  if(_und.isArray(this.streams)) return true;
+  else return false;
+};
+
 StreamConcat.prototype.addStream = function(newStream) {
-  if (this.canAddStream)
+  if (this._canAddStream(newStream))
     this.streams.push(newStream);
   else
-    this.emit('error', new Error('Can\'t add stream.'));
+    this.emit('error', new StreamError('Can\'t add stream.'));
 };
 
 module.exports = StreamConcat;
